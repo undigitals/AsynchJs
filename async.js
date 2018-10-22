3ca378266848d379245da03f1fa4d958d0dcd9ed
Umid Negmatullayev <undigitals@gmail.com>
2018-10-23 08:02:51 +0900

    replacing with jquery

--- async.js
+++ async.js
@@ -4,16 +4,20 @@ window.onload = function(){
 
     http.onreadystatechange = function(){
         if(http.readyState == 4 && http.status == 200){
-            console.log(JSON.parse(http.response));
+            // console.log(JSON.parse(http.response));
         }
     }
 
-    http.open("GET", "data/tweets.json", false);
+    http.open("GET", "data/tweets.json", true);
     http.send();
+    // console.log("test");
+
+    // jquery method
+    $.get("data/tweets.json", function(data){
+        console.log(data);
+    });
     console.log("test");
-
-
-}
+};
 
 /* Ready States
 
