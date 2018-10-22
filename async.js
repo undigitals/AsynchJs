b5adb305b0b83828e6718f8df24c51cc58f18467
Umid Negmatullayev <undigitals@gmail.com>
2018-10-22 19:36:20 +0900

    checking the condition of states

--- async.js
+++ async.js
@@ -3,7 +3,9 @@ window.onload = function(){
     var http = new XMLHttpRequest();
 
     http.onreadystatechange = function(){
-        console.log(http);
+        if(http.readyState == 4 && http.status == 200){
+            console.log(http.response);
+        }
     }
 
     http.open("GET", "data/tweets.json", true);
