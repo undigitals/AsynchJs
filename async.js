b2d9eb28f46e4ec3bec8dc730a6ff698201fbe4a
Umid Negmatullayev <undigitals@gmail.com>
2018-10-23 07:47:11 +0900

    testing, async action, json.parse()

--- async.js
+++ async.js
@@ -4,13 +4,13 @@ window.onload = function(){
 
     http.onreadystatechange = function(){
         if(http.readyState == 4 && http.status == 200){
-            console.log(http.response);
+            console.log(JSON.parse(http.response));
         }
     }
 
     http.open("GET", "data/tweets.json", true);
     http.send();
-    //console.log(http);
+    console.log("test");
 
 
 }
