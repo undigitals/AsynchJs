373f550099aab3d69db26cf292df9baee844638a
Umid Negmatullayev <undigitals@gmail.com>
2018-10-27 01:18:14 +0900

    new js promises

--- async.js
+++ async.js
@@ -25,6 +25,15 @@ window.onload = function(){
     var promise = get("data/tweets.json");
     promise.then(function(tweets){
         console.log(tweets);
+
+        return get("data/friends.json");
+    }).then(function(friends){
+        console.log(friends);
+
+        return get("data/videos.json");
+    }).then(function(videos){
+        console.log(videos);
+
     }).catch(function(error){
         console.log(error);
     });
