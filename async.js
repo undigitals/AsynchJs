91db26f785afa777d5f2d0d26c1aac67dec1685e
Umid Negmatullayev <undigitals@gmail.com>
2018-10-26 23:48:59 +0900

    callback better way

--- async.js
+++ async.js
@@ -1,37 +1,41 @@
 window.onload = function(){
 
+    function handleError(jqXHR, textStatus, error){
+        console.log(error);
+    };
+
     $.ajax({
         type: "GET",
         url: "data/tweets.json",
-        success: function(data){
-            console.log(data);
-
-            $.ajax({
-                type: "GET",
-                url: "data/friends.json",
-                success: function(data){
-                    console.log(data);
-
-                    $.ajax({
-                        type: "GET",
-                        url: "data/videos.json",
-                        success: function(data){
-                            console.log(data);
-                        },
-                        error: function(jqXHR, textStatus, error){
-                            console.log(error);
-                        }
-                    });
-                },
-                error: function(jqXHR, textStatus, error){
-                    console.log(error);
-                }
-            });
-        },
-        error: function(jqXHR, textStatus, error){
-            console.log(error);
-        }
+        success: cbTweets,
+        error: handleError
     });
+
+    function cbTweets(data){
+        console.log(data);
+    
+        $.ajax({
+            type: "GET",
+            url: "data/friends.json",
+            success: cbFriends,
+            error: handleError
+        });
+
+      }      
+      
+    function cbFriends(data){
+        console.log(data);
+
+        $.ajax({
+            type: "GET",
+            url: "data/videos.json",
+            success: function(data){
+                console.log(data);
+            },
+            error: handleError
+        });
+    }
+        
 };
 
 
